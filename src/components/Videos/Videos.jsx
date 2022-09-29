679a6037664d081655edd9c4dae3384e06d361aa
kmcclendon <kmcclendon56@gmail.com>
2022-09-29 11:41:54 -0500

    Added an api variable.

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -15,7 +15,7 @@ export default function Videos() {
     }
     function loadClient() {
         console.log("Loading")
-        gapi.client.setApiKey('AIzaSyD74LzH22H2C9X3BVHs0QAunwrsKLKLDvM');
+        gapi.client.setApiKey(API_KEY);
         return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
             .then(function () { console.log("GAPI client loaded for API"); },
                 function (err) { console.error("Error loading GAPI client for API", err); });
@@ -68,18 +68,18 @@ export default function Videos() {
     return (
         <>
         <Button style={{color: 'red'}} onClick={showVideos}>Show Videos</Button>
-
         { videoList.map ? videoList.map((item) => {
             return (
+                <Card.Content textAlign="left">
                 <div>
                     <p style={{color: 'red'}}>
-                        {/* {item.snippet.thumbnails.default.url} */}
                         {item.snippet.title}
                     </p>
                     <a href={"https://www.youtube.com/watch?v=" + item.snippet.resourceId.videoId}>
                     <img src={item.snippet.thumbnails.default.url}></img>
                     </a>
                 </div>
+                </Card.Content>
             )
         }) : null }
         </>
